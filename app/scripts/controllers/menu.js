bad91e0ce91c207ecf84ee2271fc0f11f5f848eb
natacha romano <romano.n@hotmail.com>
2018-11-27 19:14:44 -0200

    using directives

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,8 +9,32 @@
  */
 angular.module('udaciMealsApp')
   .controller('MenuCtrl', function () {
-    this.id = 'strawberry-pudding';
-    this.name = 'Strawberry Pudding';
-    this.img = 'strawberry-pudding.jpg';
-    this.rating = 5;
+    this.items = [
+      {
+        id: 'strawberry-pudding',
+        name: 'Strawberry Pudding',
+        img: 'strawberry-pudding.jpg',
+        rating: 5,
+      },
+      {
+        id: 'chicken-pomegranate-salad',
+        name: 'Chicken Pomegranate Salad',
+        img: 'chicken-pomegranate-salad.jpg',
+        rating: 4.1,
+      },
+      {
+        id: 'ham-goat-cheese-croissant',
+        name: 'Ham Goat Cheese Croissant',
+        img: 'ham-goat-cheese-croissant.jpg',
+        rating: 3.9,
+      }
+    ];
+
+    this.increment = function(item) {
+      item.rating = ((item.rating * 10) + 1) / 10;
+    };
+
+    this.decrement = function(item) {
+      item.rating = ((item.rating * 10) - 1) / 10;
+    };
   });
